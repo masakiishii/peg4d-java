d9ce7d3773e21b8855a1173fc6007e70d23d8c4f
masakiishii <masaki.ishii511@gmail.com>
2014-09-08 17:46:18 +0900

    build database

--- src/org/peg4d/DBBuilder.java
+++ src/org/peg4d/DBBuilder.java
@@ -1,27 +1,47 @@
 package org.peg4d;
 
 import java.util.ArrayList;
+
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.HashMap;
 import java.util.Map;
 
+import java.sql.*;
 
 public class DBBuilder {
 
-	class TableData {
+	class TableData implements Comparator<TableData> {
 		private String tag   = null;
 		private String value = null;
 		private int ltpos    = -1;
 		private int rtpos    = -1;
 		private int depth    = -1;
+		public TableData() {
+		}
 		public TableData(ParsingObject node, int depth, int currentpos) {
 			this.tag   = node.getTag().toString();
-			this.value = node.size() == 0 ? node.getText() : null;
+			this.value = (node.size() == 0 && node.getText().length() < 64) ? node.getText() : null;
 			this.ltpos = currentpos;
 			this.depth = depth;
 		}
 		public void setRightPostion(int rtpos) {
 			this.rtpos = rtpos;
+		}
+		@Override
+		public int compare(TableData data1, TableData data2) {
+			int ltpos1 = data1.ltpos;
+			int ltpos2 = data2.ltpos;
+			if(ltpos1 > ltpos2) {
+				return 1;
+			}
+			else if(ltpos1 == ltpos2) {
+				return 0;
+			}
+			else {
+				return -1;
+			}
 		}
 	}
 	
@@ -41,6 +61,9 @@ public class DBBuilder {
 		tabledata.setRightPostion(this.currentpos++);
 		this.datalist.add(tabledata);
 	}
+	private void sortList() {
+		Collections.sort(this.datalist, new TableData());
+	}
 	private void showDataList() {
 		System.out.println(" Tag  |  Value |  lt |  rt  |  depth");
 		System.out.println("=====================================");
@@ -53,9 +76,52 @@ public class DBBuilder {
 		}
 	}
 	
+	private void executeInsertSQL(String table, Statement stmt) {
+		try {
+			String pre = "INSERT INTO " + table + " VALUES ";
+			String sql = "";
+			TableData td;
+			for(int index = 0; index < this.datalist.size(); index++) {
+				td = this.datalist.get(index);
+				sql = pre + "( '" + td.tag + "', '" + td.value + "', " + td.ltpos + ", " + td.rtpos + ", " + td.depth + ");";
+				stmt.execute(sql);
+			}
+		} catch (Exception e){
+			System.out.println("Exception：" + e);
+		}
+	}
+
+	private void buildDataBase() {
+		String msg = "";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			msg = "Success!!";
+			Connection con   = DriverManager.getConnection( "jdbc:mysql://localhost:3306/peg4dDB", "masaki","masaki");
+			Statement  stmt  = con.createStatement();
+			String     table = "parsingObjectDB";
+			String     sql   = "CREATE TABLE " + table + "( "
+					+ "tag VARCHAR(16),"
+					+ "value VARCHAR(256),"
+					+ "lt INT PRIMARY KEY,"
+					+ "rt INT,"
+					+ "depth INT );";
+			stmt.execute(sql);
+
+			executeInsertSQL(table, stmt);
+			stmt.close();
+		} catch (ClassNotFoundException e){
+			msg = "Fail!!";
+		} catch (Exception e){
+			System.out.println("Exception：" + e);
+		}
+		System.out.println(msg);
+	}
+
 	public void build(ParsingObject root) {
 		this.parseAST(root, 0);
-		this.showDataList();
+		this.sortList();
+		//this.showDataList();
+		this.buildDataBase();
 		System.out.println("----------------------------------");
 	}
 }
