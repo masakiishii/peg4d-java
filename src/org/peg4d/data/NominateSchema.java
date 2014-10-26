1711d89b5206aa1c33ee63d8af26b45ee41cd371
masakiishii <masaki.ishii511@gmail.com>
2014-10-26 19:14:34 +0900

    stash

--- src/org/peg4d/data/NominateSchema.java
+++ src/org/peg4d/data/NominateSchema.java
@@ -58,7 +58,7 @@ public class NominateSchema {
 
 	public void nominating() {
 		ArrayList<SubNodeDataSet> list = this.relationbuilder.getSubNodeDataSetList();
-		list.sort(new SubNodeDataSet());
+		// list.sort(new SubNodeDataSet());
 		for(int i = 0; i < list.size(); i++) {
 			for(int j = i + 1; j < list.size(); j++) {
 				Set<String> setX = list.get(i).getAssumedColumnSet();
